57542b7165b504df990bb2549ca91514797cef91
edenjiga <ejimenez@dcatech.com>
2017-06-14 14:54:43 -0500

    Se agrego el login, se loguea con token, y se guarda, falta hacer el logout

--- incaribe@angular/src/app/app.module.ts
+++ incaribe@angular/src/app/app.module.ts
@@ -2,30 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { DatePickerModule } from 'ng2-datepicker';
+import { GanttModule } from 'ng2-gantt';
 
 import { AppComponent } from './app.component';
 import { GanttChartComponent } from './components/gantt-chart/gantt-chart.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SingleProjectComponent } from './components/single-project/single-project.component';
+import { LoginComponent } from './components/login/login.component';
 
-import { app_routing } from './app.routes';
+import { app_routing, appRoutingProviders } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     GanttChartComponent,
     ProjectsComponent,
-    SingleProjectComponent
+    SingleProjectComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    DatePickerModule,
+    GanttModule,
     app_routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- incaribe@angular/src/app/app.routes.ts
+++ incaribe@angular/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SingleProjectComponent } from './components/single-project/single-project.component';
+import { LoginComponent } from './components/login/login.component';
 
 const app_routes: Routes = [
     { path: 'projects', component: ProjectsComponent},
     { path: 'projects/:id', component: SingleProjectComponent},
-    { path: '**', pathMatch: 'full', redirectTo: 'projects' }
+    { path: '**', component: LoginComponent}
 ]
-    
+export const appRoutingProviders: any[] =[];
 export const app_routing = RouterModule.forRoot(app_routes);
